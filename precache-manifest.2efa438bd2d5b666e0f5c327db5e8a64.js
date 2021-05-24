e1fb7fe0a64bca4602e55e9df72681670a6607cb
koushik5470 <thatipallykoushik@gmail.com>
2021-05-25 01:23:12 +0530

    Updates

---
+++ precache-manifest.2efa438bd2d5b666e0f5c327db5e8a64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "741586bbba2a77f30d1b",
+    "url": "/my_portfolio/static/css/main.47ae61e2.chunk.css"
+  },
+  {
+    "revision": "741586bbba2a77f30d1b",
+    "url": "/my_portfolio/static/js/main.741586bb.chunk.js"
+  },
+  {
+    "revision": "999f202a3b79bea3d61b",
+    "url": "/my_portfolio/static/js/runtime~main.999f202a.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my_portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "05b18eb9604472410d3a2e5b28f96038",
+    "url": "/my_portfolio/index.html"
+  }
+];
